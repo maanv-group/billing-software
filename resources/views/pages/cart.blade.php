6f37ded0f9adfbee0d851ab06114e3d49102168f
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-07-30 22:28:47 +0530

    Cart Minor Changes

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -65,7 +65,7 @@
                             </td>
                             <td>
                                 <button type="button" class="btn btn-danger" onclick="deleteNode(this)">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                         stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
                                         <polyline points="3 6 5 6 21 6"></polyline>
